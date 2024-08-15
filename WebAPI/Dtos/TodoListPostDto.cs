f73bd94aedf9787ef75b6b67e70675530afad468
gh242 <gh242@msn.com>
2024-08-15 17:47:57 +0800

    48.【8.模型資料驗證】ASP.NET Core Web API 入門教學(8_4) - 繼承抽象化驗證資料類別

--- WebAPI/Dtos/TodoListPostDto.cs
+++ WebAPI/Dtos/TodoListPostDto.cs
@@ -1,6 +1,7 @@
 ﻿using WebAPI.Models;
 using System.ComponentModel.DataAnnotations;
 using WebAPI.ValidationAttributes;
+using WebAPI.Abstracts;
 
 namespace WebAPI.Dtos
 {
@@ -8,72 +9,8 @@ namespace WebAPI.Dtos
     //https://learn.microsoft.com/zh-tw/aspnet/core/mvc/models/validation?view=aspnetcore-8.0
     //[StartEnd]
     //[Test(Tvalue = "321")]
-    public class TodoListPostDto: IValidatableObject
+    public class TodoListPostDto: TodoListEditDtoAbstract
     {
-        // 驗證錯誤訊息
-        //"errors": {
-        //     "Name": [
-        //         "The Name field is required.",
-        //         "The Name field is not a valid e-mail address."
-        //         "名字請輸入電子信箱",
-        //         "The field Name must be a string with a maximum length of 1.",
-        //         "The field Name must match the regular expression '[a-z]'."
-        //     ],
-        //     "Orders": [
-        //         "The field Orders must be between 2 and 3."
-        //     ]
-        // },
 
-        //public Guid TodoId { get; set; }
-        [Required]
-        //[TodoName]
-        //[EmailAddress(ErrorMessage = "名字請輸入電子信箱")]
-        //[StringLength(1)]
-        //[RegularExpression("[a-z]")]
-        public string Name { get; set; }
-        //public DateTime InsertTime { get; set; }
-        //public DateTime UpdateTime { get; set; }
-        public bool Enable { get; set; }
-        [Range(2,9)]
-        public int Orders { get; set; }
-
-        public DateTime StartTime { get; set; }
-        public DateTime EndTime { get; set; }
-
-        //public Guid InsertEmployeeId { get; set; }
-        //public Guid UpdateEmployeeId { get; set; }
-
-        //public string InsertEmployeeName { get; set; }
-        //public string UpdateEmployeeName { get; set; }
-        public ICollection<UploadFilePostDto> UploadFiles { get; set; }
-
-        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
-        {
-            WebContext _todoContext = (WebContext)validationContext.GetService(typeof(WebContext));
-
-            var findName = from a in _todoContext.TodoList
-                           where a.Name == Name
-                           select a;
-
-            // 更新的時候，要排除自己，二次過濾(因為put會有問題)
-            var dto = validationContext.ObjectInstance;
-            if (dto.GetType() == typeof(TodoListPutDto)) //TodoListPutDto 再做二次過濾
-            {
-                var dtoUpdate = (TodoListPutDto)dto;
-                findName = findName.Where(a => a.TodoId != dtoUpdate.TodoId); // 把自己排除
-            }
-
-            //47.【8.模型資料驗證】ASP.NET Core Web API 入門教學(8_3) - 類別內自訂屬性資料驗證
-            if (findName.FirstOrDefault() != null)
-            {
-               yield return new ValidationResult("已存在相同的代辦事項", new string[] { "Name" }); 
-               // yield 主要是可以回傳多個return
-            }
-
-            if (StartTime >= EndTime)
-            {
-                yield return new ValidationResult("開始時間不可以大於結束時間", new string[] { "Time" });
-            }
-        }
     }
 }
--- WebAPI/Dtos/TodoListPutDto.cs
+++ WebAPI/Dtos/TodoListPutDto.cs
@@ -1,26 +1,27 @@
 ﻿using WebAPI.Models;
 using System.ComponentModel.DataAnnotations;
 using WebAPI.ValidationAttributes;
+using WebAPI.Abstracts;
 
 namespace WebAPI.Dtos
 {
     //33.【5.新增資料POST】ASP.NET Core Web API 入門教學(5_8) - 資料驗證
     //https://learn.microsoft.com/zh-tw/aspnet/core/mvc/models/validation?view=aspnetcore-8.0
-    public class TodoListPutDto
+    public class TodoListPutDto: TodoListEditDtoAbstract
     {
         //36.【6.更新資料PUT與PATCH】ASP.NET Core Web API 入門教學(6_2) - 使用DTO更新資和架構思考
         public Guid TodoId { get; set; }
-        [TodoName]
-        public string Name { get; set; }
+        //[TodoName]
+        //public string Name { get; set; }
 
-        public bool Enable { get; set; }
-        [Range(2,9)]
-        public int Orders { get; set; }
+        //public bool Enable { get; set; }
+        //[Range(2,9)]
+        //public int Orders { get; set; }
 
-        public ICollection<UploadFilePostDto> UploadFiles { get; set; }
+        //public ICollection<UploadFilePostDto> UploadFiles { get; set; }
 
-        public TodoListPutDto() { 
-            UploadFiles = new List<UploadFilePostDto>();
-        }
+        //public TodoListPutDto() { 
+        //    UploadFiles = new List<UploadFilePostDto>();
+        //}
     }
 }
