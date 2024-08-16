c4911c8c37009635abd2cc29c2387301a7cf7a84
gh242 <gh242@msn.com>
2024-08-16 15:15:33 +0800

    56.【10.上傳檔案API】ASP.NET Core Web API 入門教學(10_1) - 基本上傳檔案

---
+++ WebAPI/Controllers/FileUploadController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileUploadController : ControllerBase
+    {
+        private readonly IWebHostEnvironment _env;
+        public FileUploadController(IWebHostEnvironment env)
+        {
+            _env = env;
+        }
+        //56.【10.上傳檔案API】ASP.NET Core Web API 入門教學(10_1) - 基本上傳檔案
+        [HttpPost]
+        public void Post(ICollection<IFormFile> files)
+        {
+            var rootPath = _env.ContentRootPath + @"\\wwwroot\\";
+
+            foreach (var file in files)
+            {
+                if (file.Length > 0)
+                {
+                    var filePath = file.FileName;
+                    using (var stream = System.IO.File.Create(rootPath + filePath))
+                    {
+                        file.CopyTo(stream);
+                    }
+                }
+            }
+        }
+
+    }
+}
--- WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
+++ WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10c4c14ad09f80bd118de4920bda236cc43c7570")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+138c2ba063e461df3b1eb7052cd671f790926a5a")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
